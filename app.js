7b8529e8546c2b4183fd9bdfa41507ac6c2f8ee9
Rishal <Rishal.mhd009@gmail.com>
2023-04-25 21:44:12 +0530

    color changing done, next to loading image

--- app.js
+++ app.js
@@ -3,19 +3,26 @@ const blueButton = document.querySelector(".blue");
 const yellowButton = document.querySelector(".yellow");
 const fileInput = document.querySelector("input[type='file']");
 const previewImage = document.querySelector("#preview");
-
-pinkButton.addEventListener("click", function() {
-  previewImage.src = "./Pink umbrella.png";
-//   previewImage.style.display = "block";
-});
+const fileBox = document.querySelector('label')
 
 blueButton.addEventListener("click", function() {
   previewImage.src = "./Blue umbrella.png";
+  document.body.style.backgroundColor = 'rgb(172, 236, 252)';
+  fileBox.style.backgroundColor = 'rgb(29, 127, 255)';
+//   previewImage.style.display = "block";
+});
+
+pinkButton.addEventListener("click", function() {
+  previewImage.src = "./Pink umbrella.png";
+  document.body.style.backgroundColor = 'rgb(255, 196, 247)';
+  fileBox.style.backgroundColor = 'rgb(232, 84, 108)';
 //   previewImage.style.display = "block";
 });
 
 yellowButton.addEventListener("click", function() {
   previewImage.src = "Yello umbrella.png";
+  document.body.style.backgroundColor = 'rgb(254, 255, 196)';
+  fileBox.style.backgroundColor = 'rgb(215, 215, 48)';
 //   previewImage.style.display = "block";
 });
 
