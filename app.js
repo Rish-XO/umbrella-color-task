9e945d92ad7f46d109b8698c29e2cd96eaa0973c
Rishal <Rishal.mhd009@gmail.com>
2023-04-26 00:16:12 +0530

    commit before adding loader svg

--- app.js
+++ app.js
@@ -6,29 +6,29 @@ const fileBox = document.querySelector('label')
 const logoInput = document.getElementById('file');
 const logoPreview = document.getElementById('logo-preview');
 
+
+// changing color of body and image 
+
 blueButton.addEventListener("click", function() {
   previewImage.src = "./Blue umbrella.png";
   document.body.style.backgroundColor = 'rgb(172, 236, 252)';
   fileBox.style.backgroundColor = 'rgb(29, 127, 255)';
-//   previewImage.style.display = "block";
 });
 
 pinkButton.addEventListener("click", function() {
   previewImage.src = "./Pink umbrella.png";
   document.body.style.backgroundColor = 'rgb(255, 196, 247)';
   fileBox.style.backgroundColor = 'rgb(232, 84, 108)';
-//   previewImage.style.display = "block";
 });
 
 yellowButton.addEventListener("click", function() {
   previewImage.src = "Yello umbrella.png";
   document.body.style.backgroundColor = 'rgb(254, 255, 196)';
   fileBox.style.backgroundColor = 'rgb(215, 215, 48)';
-//   previewImage.style.display = "block";
 });
 
 
-
+// previewing the logo
 logoInput.addEventListener('change', function(e) {
   const file = this.files[0];
   const reader = new FileReader();
@@ -38,4 +38,5 @@ logoInput.addEventListener('change', function(e) {
   reader.readAsDataURL(file);
   const fileName = e.target.files[0].name;
   fileBox.innerText = fileName;
+  logoPreview.style.display="flex";
 });
